cd456ad1be63c59bc1697dbc8ff7724ef018a70c
danielonsombi <danonsombi@gmail.com>
2025-05-29 19:56:19 +0300

    Pricing Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
 import Workflow from "./components/Workflow";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
        <HeroSection/>
        <FeatureSection/>
        <Workflow/>
+       <Pricing/>
       </div>
     </>
   )
